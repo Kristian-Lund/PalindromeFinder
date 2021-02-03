976681be95b6d5841fbff374adb0e298e5c1cd63
kristian-lund <kristian.lund@zisson.com>
2021-02-03 17:51:06 +0100

    fixed failing test.. 2.nd try

--- test_is_palindrome.py
+++ test_is_palindrome.py
@@ -8,5 +8,5 @@ class TestIs_palindrome(TestCase):
   def test_is_palindrome_2(self):
     self.assertEqual(is_palindrome("Never odd or even"), True)
   def test_is_palindrome_3(self):
-    self.assertEqual(is_palindrome("GNever odd or evenG"), True)
+    self.assertEqual(is_palindrome("GNever odd or eveng"), True)
 
