9fb8573190c1e6364a7b461d1614d86dc40a7b57
kristian-lund <kristian.lund@zisson.com>
2021-02-07 18:32:03 +0100

    Reviewed some tests

--- Tests/test_is_palindrome.py
+++ Tests/test_is_palindrome.py
@@ -26,15 +26,19 @@ class TestIs_palindrome(TestCase):
   def test_Spes_Character_Late(self):
     self.assertEqual(is_palindrome("Agnes i ,senga"), True)
 
-  #The following should be true
-  def test_End_Must_be_Lowercase(self):
+  #The following should maybe be true
+  def test_CapitolLetters(self):
     self.assertEqual(is_palindrome("NEVER ODD OR EVEN"), False)
   def test_Numbers(self):
     self.assertEqual(is_palindrome("11223eLe32211"), False)
   def test_Remove_Spes_Characters_Front(self):
     self.assertEqual(is_palindrome(" Never odd or even"), False)
+  def test_With_Quotes(self):
+    self.assertEqual(is_palindrome("\"Never odd or even\""), False)
 
   #Negative Tests
+  def test_Non_Palindrome(self):
+    self.assertEqual(is_palindrome("Taxicar"), False)
   def test_Non_Palindrome(self):
     self.assertEqual(is_palindrome("Never Even or odd"), False)
   # The following should be false
